9b78961ee02afcec001220b8d29f70eb610bc24f
Shahid Dhariwala <shahid.dhariwala@gmail.com>
2023-09-19 01:29:55 +0530

    Utils to split added

--- utils.py
+++ utils.py
@@ -5,11 +5,11 @@ def split_csv(num_splits=2):
     csv_df = pd.read_csv("quotes-500k-webscrapped.csv")
     bucket_len = len(csv_df) // num_splits
 
-    for i in range(len(csv_df)):
-        csv_df.iloc[:, i*bucket_len:i +
-                    bucket_len].to_csv(f"quotes-500k-webscrapped-{i+1}.csv")
-
-    print(bucket_len)
+    for i in range(num_splits):
+        print(f"{i*bucket_len}- {i*bucket_len + bucket_len}")
+        df = csv_df[ i*bucket_len:i*bucket_len +
+                         bucket_len]
+        df.to_csv(f"quotes-500k-webscrapped-{i+1}.csv")
 
 
 split_csv(2)
